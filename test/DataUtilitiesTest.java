315af85dc0ae312d1b7a178d44c068ae7fc92275
ESDiep <eric.diepvn@gmail.com>
2024-03-11 14:20:00 -0600

    adding assignment

---
+++ test/DataUtilitiesTest.java
@@ -0,0 +1,16 @@
+package org.jfree.data.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculateColumnTotalTest.class,
+        CalculateRowTotalTest.class,
+        CreateNumberArrayTest.class,
+        CreateNumberArray2DTest.class,
+        GetCumulativePercentagesTest.class,
+
+})
+public class DataUtilitiesTest {
+}
---
+++ test/RangeTest.java
@@ -0,0 +1,17 @@
+package org.jfree.data.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		
+		ContainsTest.class,
+		GetLengthTest.class,
+		GetCentralValueTest.class,
+		GetLowerBoundTest.class,
+		GetUpperBoundTest.class,
+})
+public class RangeTest {
+
+}
